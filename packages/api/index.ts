fb250b50bfd22f29152beda7758a74e3ad57e0bf
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-01-22 16:03:36 +0000

    add dummy response to lambda

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -4,15 +4,22 @@ import {
 	APIGatewayProxyEvent,
 	APIGatewayEventRequestContext,
 	APIGatewayProxyCallback,
+	APIGatewayProxyStructuredResultV2,
 } from 'aws-lambda';
 
 export const handler = (
 	event: APIGatewayProxyEvent,
 	context: APIGatewayEventRequestContext,
 	callback: APIGatewayProxyCallback,
-): Promise<string> => {
+): Promise<APIGatewayProxyStructuredResultV2> => {
 	const message = 'Hello World!';
 	console.log(message);
     console.log(event.body, context.accountId, callback.name);
-	return Promise.resolve(message);
+	const result = {
+		"isBase64Encoded": true,
+		"statusCode": 200,
+		"headers": {  },
+		"body": message
+	}
+	return Promise.resolve(result);
 };
