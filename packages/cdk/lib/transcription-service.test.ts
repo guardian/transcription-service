e114afc931a2cf2fb52bf9b59d84a8e75a23f791
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2024-01-22 11:22:44 +0000

    Initial CDK package (#1)
    * remove contents of node_modules
    
    * initialise cdk package
    
    npx @guardian/cdk@latest new --app transcription-service --stack investigations --stage CODE --stage PROD --package-manager npm
    
    * add cdk scripts to root package.json

---
+++ packages/cdk/lib/transcription-service.test.ts
@@ -0,0 +1,12 @@
+import { App } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { TranscriptionService } from "./transcription-service";
+
+describe("The TranscriptionService stack", () => {
+  it("matches the snapshot", () => {
+    const app = new App();
+    const stack = new TranscriptionService(app, "TranscriptionService", { stack: "investigations", stage: "TEST" });
+    const template = Template.fromStack(stack);
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+});
