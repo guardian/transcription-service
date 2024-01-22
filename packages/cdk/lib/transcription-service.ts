9d5b9840ced2e07cf2cc8f6196c28b8ef023f7eb
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2024-01-22 13:08:18 +0000

    fix lint errors

--- packages/cdk/lib/transcription-service.ts
+++ packages/cdk/lib/transcription-service.ts
@@ -1,6 +1,6 @@
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import { GuApiLambda } from "@guardian/cdk";
+import { GuStack } from "@guardian/cdk/lib/constructs/core";
+import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 
