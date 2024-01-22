61c3b7a3804b71f71b23aa527af819be1acb5b9c
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2024-01-22 12:31:04 +0000

    tweaks to prettier config, api-ci.yaml

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -3,5 +3,5 @@ import { GuRoot } from "@guardian/cdk/lib/constructs/root";
 import { TranscriptionService } from "../lib/transcription-service";
 
 const app = new GuRoot();
-new TranscriptionService(app, "TranscriptionService-euwest-1-CODE", { stack: "investigations", stage: "CODE", env: { region: "eu-west-1" } });
-new TranscriptionService(app, "TranscriptionService-euwest-1-PROD", { stack: "investigations", stage: "PROD", env: { region: "eu-west-1" } });
+new TranscriptionService(app, "TranscriptionService-CODE", { stack: "investigations", stage: "CODE", env: { region: "eu-west-1" } });
+new TranscriptionService(app, "TranscriptionService-PROD", { stack: "investigations", stage: "PROD", env: { region: "eu-west-1" } });
