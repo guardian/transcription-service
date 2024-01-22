ce7e21d76967e6386595be9248248bc5fe039f0b
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-01-22 12:51:14 +0000

    change target dir

--- packages/cdk/lib/transcription-service.ts
+++ packages/cdk/lib/transcription-service.ts
@@ -1,9 +1,27 @@
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
+import { GuApiLambda } from "@guardian/cdk";
 import type { App } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class TranscriptionService extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+
+    const APP_NAME = "transcription-service";
+
+    new GuApiLambda(this, "transcription-service-api", {
+      fileName: "api.zip",
+      handler: "index.js",
+      runtime: Runtime.NODEJS_20_X,
+      monitoringConfiguration: {
+        noMonitoring: true,
+      },
+      app: APP_NAME,
+      api: {
+        id: "my-api",
+        description: "...",
+      },
+    });
   }
 }
