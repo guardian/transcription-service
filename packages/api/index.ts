8c3b633d99226747f848d08d697a474251573b85
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-01-23 09:13:00 +0000

    add a health check

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,10 +1,13 @@
-import {
-	APIGatewayProxyEvent,
-	APIGatewayEventRequestContext,
-	APIGatewayProxyCallback,
-	APIGatewayProxyStructuredResultV2,
-} from 'aws-lambda';
-import serverlessExpress from "@codegenie/serverless-express"
+// import {
+// 	APIGatewayProxyEvent,
+// 	APIGatewayEventRequestContext,
+// 	APIGatewayProxyCallback,
+// 	APIGatewayProxyStructuredResultV2,
+// } from 'aws-lambda';
+import express from 'express';
+import asyncHandler from "express-async-handler";
+import serverlessExpress from "@codegenie/serverless-express";
+import bodyParser from 'body-parser';
 
 // export const handler = (
 // 	event: APIGatewayProxyEvent,
@@ -23,12 +26,48 @@ import serverlessExpress from "@codegenie/serverless-express"
 // 	return Promise.resolve(result);
 // };
 
-let api
+const getApp = async () => {
+	const app = express();
+	const apiRouter = express.Router();
+
+	app.use(bodyParser.json({ limit: "40mb" }));
+
+	apiRouter.get(
+		"/healthcheck",
+		asyncHandler(async (req, res) => {
+			res.send("It lives!")
+		})
+	);
+
+	app.use("/api", apiRouter);
+
+	return app;
+}
+
+let api;
 if (process.env["AWS_EXECUTION_ENV"] !== undefined){
 	console.log("Running on lambda")
 
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	let serverlessExpressHandler: any
-	const serverlessHandler = getApp().then((app) => (serverlessExpressHandler = serverlessExpress({ app }.handler)))
+	const serverlessHandler = getApp().then((app) => (serverlessExpressHandler = serverlessExpress({ app }).handler));
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	api = async (event: any, context: any) => {
+		if (!serverlessExpressHandler) {
+			await serverlessHandler;
+		}
+		return serverlessExpressHandler(event, context)
+	}
 } else {
-	console.log("running locally")
+	console.log("running locally");
+	// Running locally. Start Express ourselves
+	const port = 9103
+	getApp().then((app) => {
+		app.listen(port, () => {
+			console.log(`Server now listening on port: ${port}`)
+		})
+	})
 }
+
+export { api }
