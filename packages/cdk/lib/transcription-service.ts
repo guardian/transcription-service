58ed155d1fe3ae1e22f8cf8937d22cc02588e5d3
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-01-23 18:15:54 +0000

    add ssm read policy to api lambda

--- packages/cdk/lib/transcription-service.ts
+++ packages/cdk/lib/transcription-service.ts
@@ -3,6 +3,8 @@ import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { GuardianAwsAccounts } from "@guardian/private-infrastructure-config";
 
 export class TranscriptionService extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
@@ -10,8 +12,9 @@ export class TranscriptionService extends GuStack {
 
     const APP_NAME = "transcription-service";
     const apiId = `${APP_NAME}-${props.stage}`
+    const ssmPrefix = `arn:aws:ssm:${props.env.region}:${GuardianAwsAccounts.Investigations}:parameter`;
 
-    new GuApiLambda(this, "transcription-service-api", {
+    const apiLambda = new GuApiLambda(this, "transcription-service-api", {
       fileName: "api.zip",
       handler: "index.api",
       runtime: Runtime.NODEJS_20_X,
@@ -24,5 +27,11 @@ export class TranscriptionService extends GuStack {
         description: "API for transcription service frontend",
       },
     });
+
+    apiLambda.addToRolePolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["ssm:GetParameter", "ssm:GetParametersByPath"],
+      resources: [`${ssmPrefix}/${this.stage}/${this.stack}/${APP_NAME}/*`],
+    }));
   }
 }
