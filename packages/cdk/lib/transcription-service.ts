d35c8875eea83c4b9715cf336a8c6a96da6773a3
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2024-01-24 15:14:18 +0000

    wip add domain name

--- packages/cdk/lib/transcription-service.ts
+++ packages/cdk/lib/transcription-service.ts
@@ -1,39 +1,60 @@
-import { GuApiLambda } from "@guardian/cdk";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuardianAwsAccounts } from "@guardian/private-infrastructure-config";
-import type { App } from "aws-cdk-lib";
-import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { GuApiLambda } from '@guardian/cdk';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStringParameter } from '@guardian/cdk/lib/constructs/core/parameters';
+import { GuardianAwsAccounts } from '@guardian/private-infrastructure-config';
+import type { App } from 'aws-cdk-lib';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export class TranscriptionService extends GuStack {
-  constructor(scope: App, id: string, props: GuStackProps) {
-    super(scope, id, props);
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
 
-    const APP_NAME = "transcription-service";
-    const apiId = `${APP_NAME}-${props.stage}`;
-    if (!props.env?.region) throw new Error("region not provided in props");
-    
-    const ssmPrefix = `arn:aws:ssm:${props.env.region}:${GuardianAwsAccounts.Investigations}:parameter`;
+		const APP_NAME = 'transcription-service';
+		const apiId = `${APP_NAME}-${props.stage}`;
+		if (!props.env?.region) throw new Error('region not provided in props');
 
-    const apiLambda = new GuApiLambda(this, "transcription-service-api", {
-      fileName: "api.zip",
-      handler: "index.api",
-      runtime: Runtime.NODEJS_20_X,
-      monitoringConfiguration: {
-        noMonitoring: true,
-      },
-      app: APP_NAME,
-      api: {
-        id: apiId,
-        description: "API for transcription service frontend",
-      },
-    });
+		const ssmPrefix = `arn:aws:ssm:${props.env.region}:${GuardianAwsAccounts.Investigations}:parameter`;
 
-    apiLambda.addToRolePolicy(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ["ssm:GetParameter", "ssm:GetParametersByPath"],
-      resources: [`${ssmPrefix}/${this.stage}/${this.stack}/${APP_NAME}/*`],
-    }));
-  }
+		const ssmPath = `${this.stage}/${this.stack}/${APP_NAME}`;
+
+		const certificateId = new GuStringParameter(this, 'certificateId', {
+			fromSSM: true,
+			default: `${ssmPath}/certificateId`,
+		});
+		const certificateArn = `arn:aws:acm:${props.env.region}:${GuardianAwsAccounts.Investigations}:certificate/${certificateId}`;
+		const certificate = Certificate.fromCertificateArn(
+			this,
+			`${APP_NAME}-certificate`,
+			certificateArn,
+		);
+		const domainName = 'discourse.code.dev-gutools.co.uk';
+
+		const apiLambda = new GuApiLambda(this, 'transcription-service-api', {
+			fileName: 'api.zip',
+			handler: 'index.api',
+			runtime: Runtime.NODEJS_20_X,
+			monitoringConfiguration: {
+				noMonitoring: true,
+			},
+			app: APP_NAME,
+			api: {
+				id: apiId,
+				description: 'API for transcription service frontend',
+				domainName: {
+					certificate,
+				},
+			},
+		});
+
+		apiLambda.addToRolePolicy(
+			new PolicyStatement({
+				effect: Effect.ALLOW,
+				actions: ['ssm:GetParameter', 'ssm:GetParametersByPath'],
+				resources: [`${ssmPrefix}/${ssmPath}/*`],
+			}),
+		);
+	}
 }
