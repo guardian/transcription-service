98a046fdc40449956b75b38c6cbb8fbc0a329fc2
philmcmahon <philip.mcmahon@theguardian.com>
2024-01-26 16:51:52 +0000

    Tidy up cdk

--- packages/cdk/lib/transcription-service.ts
+++ packages/cdk/lib/transcription-service.ts
@@ -1,13 +1,23 @@
-import { GuApiLambda } from '@guardian/cdk';
-import { GuCertificate } from '@guardian/cdk/lib/constructs/acm';
-import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
-import { GuCname } from '@guardian/cdk/lib/constructs/dns';
-import { GuardianAwsAccounts } from '@guardian/private-infrastructure-config';
-import { type App, Duration } from 'aws-cdk-lib';
-import { EndpointType } from 'aws-cdk-lib/aws-apigateway';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import {GuApiLambda} from '@guardian/cdk';
+import {GuCertificate} from '@guardian/cdk/lib/constructs/acm';
+import type {GuStackProps} from '@guardian/cdk/lib/constructs/core';
+import {GuAmiParameter, GuDistributionBucketParameter, GuStack} from '@guardian/cdk/lib/constructs/core';
+import {GuCname} from '@guardian/cdk/lib/constructs/dns';
+import {GuVpc, SubnetType} from "@guardian/cdk/lib/constructs/ec2";
+import {GuardianAwsAccounts} from '@guardian/private-infrastructure-config';
+import {type App, Duration} from 'aws-cdk-lib';
+import {EndpointType} from 'aws-cdk-lib/aws-apigateway';
+import {AutoScalingGroup, BlockDeviceVolume, SpotAllocationStrategy} from "aws-cdk-lib/aws-autoscaling";
+import {
+	InstanceClass,
+	InstanceSize,
+	InstanceType,
+	LaunchTemplate,
+	MachineImage,
+	UserData
+} from "aws-cdk-lib/aws-ec2";
+import {Effect, PolicyStatement} from 'aws-cdk-lib/aws-iam';
+import {Runtime} from 'aws-cdk-lib/aws-lambda';
 
 export class TranscriptionService extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
@@ -15,7 +25,13 @@ export class TranscriptionService extends GuStack {
 
 		const APP_NAME = 'transcription-service';
 		const apiId = `${APP_NAME}-${props.stage}`;
+		const isProd = props.stage === 'PROD';
 		if (!props.env?.region) throw new Error('region not provided in props');
+
+		const workerAmi = new GuAmiParameter(this, {
+			app: `${APP_NAME}-worker`,
+			description: "AMI to use for the worker instances"
+		})
 
 		const ssmPrefix = `arn:aws:ssm:${props.env.region}:${GuardianAwsAccounts.Investigations}:parameter`;
 		const ssmPath = `${this.stage}/${this.stack}/${APP_NAME}`;
@@ -67,5 +83,79 @@ export class TranscriptionService extends GuStack {
 			ttl: Duration.hours(1),
 			resourceRecord: apiDomain.domainNameAliasDomainName,
 		});
+
+		// worker
+
+		const workerApp = `${APP_NAME}-worker`
+		const userData = UserData.forLinux({ shebang: "#!/bin/bash"
+		})
+		// basic placeholder commands
+			userData.addCommands([
+			`aws s3 cp s3://${GuDistributionBucketParameter.getInstance(this).valueAsString}/${props.stack}/${props.stage}/${APP_NAME}/worker.zip`,
+				`unzip worker.zip`,
+				`node index.js`
+		].join("\n"))
+
+		const launchTemplate = new LaunchTemplate(this, "TranscriptionWorkerLaunchTemplate", {
+			machineImage: MachineImage.genericLinux({"eu-west-1": workerAmi.valueAsString}),
+			instanceType: InstanceType.of(InstanceClass.C7G, InstanceSize.XLARGE4),
+			// the size of this block device will determine the max input file size for transcription. In future we could
+			// attach the block device on startup once we know how large the file to be transcribed is, or try some kind
+			// of streaming approach to the transcription so we don't need the whole file on disk
+			blockDevices: [
+				{
+					deviceName: "/dev/sda1",
+					// assuming that we intend to support video files, 50GB seems a reasonable starting point (beyond that
+					// the browser upload might struggle anyway)
+					volume: BlockDeviceVolume.ebs(50)
+				}
+			],
+			userData
+		})
+
+		// instance types we are happy to use for workers. Note - order matters as when launching 'on demand' instances
+		// the ASG will start at the top of the list and work down until it manages to launch an instance
+		const acceptableInstanceTypes = [
+			InstanceType.of(InstanceClass.C7G, InstanceSize.XLARGE4),
+			InstanceType.of(InstanceClass.C6G, InstanceSize.XLARGE4),
+			InstanceType.of(InstanceClass.M7G, InstanceSize.XLARGE4),
+			InstanceType.of(InstanceClass.C7G, InstanceSize.XLARGE8),
+			InstanceType.of(InstanceClass.C6G, InstanceSize.XLARGE8)
+		]
+
+		// worker autoscaling group
+		// unfortunately GuAutoscalingGroup doesn't support having a mixedInstancesPolicy so using the basic ASG here
+		new AutoScalingGroup(this, "TransciptionWorkerASG", {
+			minCapacity: 0,
+			maxCapacity: isProd ? 20 : 4,
+			autoScalingGroupName: `transcription-service-workers-${this.stage}`,
+			vpc: GuVpc.fromIdParameter(this, "InvestigationsInternetEnabledVpc", {
+				availabilityZones: ["eu-west-1a", "eu-west-1b", "eu-west-1c"],
+			}),
+			vpcSubnets: {
+				subnets: GuVpc.subnetsFromParameter(this, {
+					type: SubnetType.PRIVATE,
+					app: workerApp
+				})
+			},
+			// instances should shut themselves down when they have finished the transcription - they should not be terminated
+			// by the ASG
+			newInstancesProtectedFromScaleIn: true,
+			mixedInstancesPolicy: {
+				launchTemplate,
+				instancesDistribution: {
+					// 0 is the default, including this here just to make it more obvious what's happening
+					onDemandBaseCapacity: 0,
+					// if this value is set to 100, then we won't use spot instances at all, if it is 0 then we use 100% spot
+					onDemandPercentageAboveBaseCapacity: 100,
+					spotAllocationStrategy: SpotAllocationStrategy.CAPACITY_OPTIMIZED,
+					spotMaxPrice: "0.6202"
+				},
+				launchTemplateOverrides: acceptableInstanceTypes.map(instanceType => ({
+					instanceType
+				}))
+			}
+		})
+
 	}
 }
