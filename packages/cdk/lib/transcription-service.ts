fec5a5b68cfefbb9c98b5e0e65a4f3a65ac89d2b
philmcmahon <philip.mcmahon@theguardian.com>
2024-01-31 23:19:15 +0000

    Set AWS_REGION env. var in transcription worker userdata

--- packages/cdk/lib/transcription-service.ts
+++ packages/cdk/lib/transcription-service.ts
@@ -103,6 +103,7 @@ export class TranscriptionService extends GuStack {
 		userData.addCommands(
 			[
 				`export STAGE=${props.stage}`,
+				`export AWS_REGION=${props.env.region}`,
 				`aws s3 cp s3://${GuDistributionBucketParameter.getInstance(this).valueAsString}/${props.stack}/${props.stage}/${workerApp}/worker.zip .`,
 				`unzip worker.zip`,
 				`node index.js`,
