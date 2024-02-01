3ec37e4727a9541f51e4de1d3a6d4dba7705ba27
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2024-02-01 16:56:32 +0000

    deduplicate configs

--- packages/cdk/target_tracking_scaling_policy/put-scaling-policy.sh
+++ packages/cdk/target_tracking_scaling_policy/put-scaling-policy.sh
@@ -6,6 +6,10 @@ if [[ $STAGE != 'CODE' && $STAGE != 'PROD' ]]; then
   exit
 fi
 
+sed "s/STAGE/$STAGE/g" $dirname/config.json > $dirname/config-$STAGE.json
+
 aws autoscaling put-scaling-policy --policy-name sqs-backlog-target-tracking-scaling-policy-$STAGE \
   --auto-scaling-group-name transcription-service-workers-$STAGE --policy-type TargetTrackingScaling \
   --target-tracking-configuration file://$dirname/config-$STAGE.json
+
+rm $dirname/config-$STAGE.json
