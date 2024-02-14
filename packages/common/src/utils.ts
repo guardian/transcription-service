db020198f669cf80f745270012fc26b03a5ed3a4
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-02-14 17:33:37 +0000

    add log to upload to S3

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -5,7 +5,14 @@ export const uploadToS3 = async (url: string, blob: Blob) => {
 			body: blob,
 		});
 		const status = response.status;
-		return status === 200;
+		const isSuccess = status === 200;
+		if (!isSuccess) {
+			console.log(`S3 upload status is ${status} - ${response.statusText}`);
+			const responseText = await response.text();
+			console.log('responseText: ', responseText);
+		}
+
+		return isSuccess;
 	} catch (error) {
 		console.error('upload error:', error);
 		return false;
