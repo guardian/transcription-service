9b378ba0bd0817ba90d2c820a2251c941d1553ce
philmcmahon <philip.mcmahon@theguardian.com>
2024-02-14 11:50:21 +0000

    Move useContext call into UploadForm rather than Home

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 import React from 'react';
 import { UploadForm } from '../components/UploadForm';
-import { useContext } from 'react';
-import { AuthContext } from './template';
-
 const Home = () => {
-	const auth = useContext(AuthContext);
 	return (
 		<div>
 			<h2>Upload file for transcribing</h2>
-			<UploadForm auth={auth}></UploadForm>
+			<UploadForm></UploadForm>
 		</div>
 	);
 };
