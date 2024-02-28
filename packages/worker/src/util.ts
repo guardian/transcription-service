9049292fc98c6a1f0d0f08738b5f707667a65950
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-02-28 13:37:41 +0000

    fix bug for logging transcribes text

--- packages/worker/src/util.ts
+++ packages/worker/src/util.ts
@@ -4,36 +4,26 @@ import {
 	uploadToS3,
 	type OutputBucketUrls,
 } from '@guardian/transcription-service-common';
-import path from 'path';
 
 export const uploadAllTranscriptsToS3 = async (
 	destinationBucketUrls: OutputBucketUrls,
 	files: Transcripts,
 ) => {
 	const getBlob = (file: string) => new Blob([file as BlobPart]);
-	const getFileName = (file: string) => path.basename(file);
 	const blobs: [string, string, Blob][] = [
-		[getFileName(files.srt), destinationBucketUrls.srt.url, getBlob(files.srt)],
-		[
-			getFileName(files.json),
-			destinationBucketUrls.json.url,
-			getBlob(files.json),
-		],
-		[
-			getFileName(files.text),
-			destinationBucketUrls.text.url,
-			getBlob(files.text),
-		],
+		['srt', destinationBucketUrls.srt.url, getBlob(files.srt)],
+		['json', destinationBucketUrls.json.url, getBlob(files.json)],
+		['text', destinationBucketUrls.text.url, getBlob(files.text)],
 	];
 
 	for (const blobDetail of blobs) {
-		const [fileName, url, blob] = blobDetail;
+		const [fileFormat, url, blob] = blobDetail;
 		const response = await uploadToS3(url, blob);
 		if (!response.isSuccess) {
 			throw new Error(
-				`Could not upload file: ${fileName} to S3! ${response.errorMsg}`,
+				`Could not upload file format: ${fileFormat} to S3! ${response.errorMsg}`,
 			);
 		}
-		logger.info(`Successfully uploaded ${fileName} to S3`);
+		logger.info(`Successfully uploaded file format ${fileFormat} to S3`);
 	}
 };
