0a028be391d13df305ae4c21d5e6229abc8547e8
philmcmahon <philip.mcmahon@theguardian.com>
2024-02-28 16:04:01 +0000

    debug

--- packages/output-handler/src/index.ts
+++ packages/output-handler/src/index.ts
@@ -94,6 +94,7 @@ const processMessage = async (event: unknown) => {
 };
 
 const handler: Handler = async (event) => {
+	console.log(event);
 	await processMessage(event);
 	return 'Finished processing Event';
 };
