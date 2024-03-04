55a555f295eb350e0b7b47eed1a9576f77b8940f
philmcmahon <philip.mcmahon@theguardian.com>
2024-03-04 14:09:30 +0000

    more error handling

--- packages/worker/src/spot-termination.ts
+++ packages/worker/src/spot-termination.ts
@@ -12,21 +12,38 @@ export const checkSpotInterrupt = async (
 	queueUrl: string,
 ) => {
 	const url = 'http://169.254.169.254/latest/meta-data/spot/instance-action';
-	const result = await fetch(url);
-	if (result.status === 200) {
-		const json = await result.json();
-		if (json.action === 'terminate') {
-			setInterruptionTime(new Date(json.time));
-			logger.warn('Spot instance termination detected');
-			// Interrupt warning occurs 2 minutes before termination
-			const receiptHandle = getCurrentReceiptHandle();
-			if (!receiptHandle) {
+	try {
+		const result = await fetch(url);
+		if (result.status === 200) {
+			const json = await result.json();
+			if (json.action === 'terminate') {
+				const interruptionTime = new Date(json.time);
+				setInterruptionTime(interruptionTime);
+				logger.warn('Spot instance termination detected');
+				// Interrupt warning occurs 2 minutes before termination
+				const receiptHandle = getCurrentReceiptHandle();
+				if (!receiptHandle) {
+					return;
+				}
+				const secondsUntilTermination =
+					(interruptionTime.getTime() - new Date().getTime()) / 1000;
+				try {
+					await changeMessageVisibility(
+						client,
+						queueUrl,
+						receiptHandle,
+						secondsUntilTermination,
+					);
+				} catch (e) {
+					// we don't care if the visibility change fails - it will just delay the message being
+					// picked up by a new worker, so do nothing here
+				}
+				// once the interrupt warning has happened, we don't need to keep checking
 				return;
 			}
-			await changeMessageVisibility(client, queueUrl, receiptHandle, 110);
-			// once the interrupt warning has happened, we don't need to keep checking
-			return;
 		}
+	} catch (e) {
+		console.error('Error during spot termination check', e);
 	}
 	setTimeout(
 		() => checkSpotInterrupt(client, queueUrl),
