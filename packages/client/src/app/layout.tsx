3ab78ab94b2492a7dab8f08b44d2ce2564a994b2
philmcmahon <philip.mcmahon@theguardian.com>
2024-06-26 14:16:31 +0100

    Add padding under main div to ensure space for footer

--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 					</div>
 				</header>
 				<main>
-					<div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 px-4">
+					<div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 px-4 pb-20">
 						{children}
 					</div>
 				</main>
