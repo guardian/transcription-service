2ad99d5e59b2caa739b4f91da081f6ce0c94ab13
Marjan Kalanaki <marjan.kalanaki@guardian.co.uk>
2024-10-04 18:38:16 +0100

    improve local queues to make them consistent with prod environment

---
+++ scripts/delete-local-queues.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+aws sqs delete-queue --queue-url http://localhost:4566/000000000000/transcription-service-task-queue-DEV.fifo --endpoint-url http://localhost:4566
+aws sqs delete-queue --queue-url http://localhost:4566/000000000000/transcription-service-output-queue-DEV --endpoint-url http://localhost:4566
+aws sqs delete-queue --queue-url http://localhost:4566/000000000000/giant-output-dead-letter-queue-DEV.fifo --endpoint-url http://localhost:4566
+aws sqs delete-queue --queue-url http://localhost:4566/000000000000/transcription-service-task-dead-letter-queue-DEV.fifo --endpoint-url http://localhost:4566
+aws sqs delete-queue --queue-url http://localhost:4566/000000000000/giant-output-queue-DEV.fifo --endpoint-url http://localhost:4566
--- scripts/purge-local-queue.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-aws sqs purge-queue --queue-url http://localhost:4566/000000000000/transcription-service-task-queue-DEV.fifo --endpoint-url http://localhost:4566
-aws sqs purge-queue --queue-url http://localhost:4566/000000000000/giant-output-queue-DEV.fifo --endpoint-url http://localhost:4566
