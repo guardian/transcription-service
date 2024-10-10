5028113ee9c332e00425bc6afa24deb372052d50
philmcmahon <philip.mcmahon@theguardian.com>
2024-10-10 12:06:18 +0100

    Send message if media download fails.

--- packages/media-download/src/index.ts
+++ packages/media-download/src/index.ts
@@ -1,9 +1,12 @@
 import {
+	deleteMessage,
 	generateOutputSignedUrlAndSendMessage,
 	getConfig,
 	getSignedDownloadUrl,
 	getSQSClient,
 	isSqsFailure,
+	logger,
+	sendMessage,
 	TranscriptionConfig,
 } from '@guardian/transcription-service-backend-common';
 import { Upload } from '@aws-sdk/lib-storage';
@@ -12,7 +15,11 @@ import { createReadStream } from 'node:fs';
 import { getNextJob } from './sqs';
 import { downloadMedia, MediaMetadata } from './yt-dlp';
 import { SQSClient } from '@aws-sdk/client-sqs';
-import { MediaDownloadJob } from '@guardian/transcription-service-common';
+import {
+	DestinationService,
+	MediaDownloadFailure,
+	MediaDownloadJob,
+} from '@guardian/transcription-service-common';
 
 const uploadToS3 = async (
 	s3Client: S3Client,
@@ -41,6 +48,28 @@ const uploadToS3 = async (
 	} catch (e) {
 		console.error(e);
 		throw e;
+	}
+};
+
+const reportDownloadFailure = async (
+	config: TranscriptionConfig,
+	sqsClient: SQSClient,
+	job: MediaDownloadJob,
+) => {
+	const mediaDownloadFailure: MediaDownloadFailure = {
+		id: job.id,
+		status: 'FAILURE',
+		url: job.url,
+	};
+	const result = await sendMessage(
+		sqsClient,
+		config.app.destinationQueueUrls[DestinationService.TranscriptionService],
+		JSON.stringify(mediaDownloadFailure),
+		job.id,
+	);
+	if (isSqsFailure(result)) {
+		logger.error('Failed to send download failure message', result.error);
+		throw new Error('Failed to send failure message');
 	}
 };
 
@@ -76,6 +105,7 @@ const requestTranscription = async (
 
 const main = async () => {
 	console.log('Starting media download service');
+	console.log(`INPUT: ${process.env.MESSAGE_BODY}`);
 	const config = await getConfig();
 
 	const s3Client = new S3Client({ region: config.aws.region });
@@ -83,20 +113,32 @@ const main = async () => {
 		config.aws.region,
 		config.aws.localstackEndpoint,
 	);
-	const job = await getNextJob(
+	const jobResult = await getNextJob(
 		sqsClient,
 		config.app.mediaDownloadQueueUrl,
 		config.app.stage === 'DEV',
 	);
-	if (job) {
+	if (jobResult && jobResult.job) {
+		const { job, receiptHandle } = jobResult;
 		const metadata = await downloadMedia(job.url, '/tmp', job.id);
-		const key = await uploadToS3(
-			s3Client,
-			metadata,
-			config.app.sourceMediaBucket,
-			job.id,
-		);
-		await requestTranscription(config, key, sqsClient, job, metadata);
+		if (!metadata) {
+			await reportDownloadFailure(config, sqsClient, job);
+		} else {
+			const key = await uploadToS3(
+				s3Client,
+				metadata,
+				config.app.sourceMediaBucket,
+				job.id,
+			);
+			await requestTranscription(config, key, sqsClient, job, metadata);
+		}
+		if (receiptHandle) {
+			await deleteMessage(
+				sqsClient,
+				config.app.mediaDownloadQueueUrl,
+				receiptHandle,
+			);
+		}
 	}
 	setTimeout(main, 1000);
 };
