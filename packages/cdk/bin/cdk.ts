bb28310df5cf4fef51cefcb021f4a7645aa76139
philmcmahon <philip.mcmahon@theguardian.com>
2025-02-04 11:57:04 +0000

    Add ffmpeg layer infra

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -2,6 +2,7 @@ import 'source-map-support/register';
 import { GuRoot } from '@guardian/cdk/lib/constructs/root';
 import { TranscriptionServiceRepository } from '../lib/repository';
 import { TranscriptionService } from '../lib/transcription-service';
+import { TranscriptionServiceUniversalInfra } from '../lib/universal-infra';
 
 const app = new GuRoot();
 new TranscriptionService(app, 'TranscriptionService-CODE', {
@@ -21,3 +22,15 @@ new TranscriptionServiceRepository(app, 'TranscriptionServiceRepository', {
 	stage: 'PROD',
 	env: { region: 'eu-west-1' },
 });
+
+// This is another stack which is used for both code/prod - but as repository already existed I made a new stack to avoid
+// having to delete the whole repository stack (including all containers) in order to give it a less specific name
+new TranscriptionServiceUniversalInfra(
+	app,
+	'TranscriptionServiceUniversalInfra',
+	{
+		stack: 'investigations',
+		stage: 'PROD',
+		env: { region: 'eu-west-1' },
+	},
+);
