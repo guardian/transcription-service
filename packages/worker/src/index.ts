72f2fe07198ad42afaf250bd8c1f4dba5735e3f1
philmcmahon <philip.mcmahon@theguardian.com>
2025-08-12 10:05:32 +0100

    Make worker always error

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -4,43 +4,43 @@ import {
 	getNextMessage,
 	parseTranscriptJobMessage,
 	isSqsFailure,
-	deleteMessage,
-	changeMessageVisibility,
-	getObjectWithPresignedUrl,
+	// deleteMessage,
+	// changeMessageVisibility,
+	// getObjectWithPresignedUrl,
 	TranscriptionConfig,
-	moveMessageToDeadLetterQueue,
+	// moveMessageToDeadLetterQueue,
 	logger,
 	publishTranscriptionOutput,
 	readFile,
 	getASGClient,
 } from '@guardian/transcription-service-backend-common';
 import {
-	DestinationService,
-	OutputBucketKeys,
-	OutputLanguageCode,
+	// DestinationService,
+	// OutputBucketKeys,
+	// OutputLanguageCode,
 	TranscriptionJob,
 	TranscriptionOutputFailure,
-	type TranscriptionOutputSuccess,
+	// type TranscriptionOutputSuccess,
 } from '@guardian/transcription-service-common';
-import {
-	getTranscriptionText,
-	runFfmpeg,
-	getOrCreateContainer,
-	WhisperBaseParams,
-	CONTAINER_FOLDER,
-	getFfmpegParams,
-} from './transcribe';
-import path from 'path';
+// import {
+// 	getTranscriptionText,
+// 	runFfmpeg,
+// 	getOrCreateContainer,
+// 	WhisperBaseParams,
+// 	CONTAINER_FOLDER,
+// 	getFfmpegParams,
+// } from './transcribe';
+// import path from 'path';
 
 import { getInstanceLifecycleState, updateScaleInProtection } from './asg';
-import { uploadAllTranscriptsToS3 } from './util';
+// import { uploadAllTranscriptsToS3 } from './util';
 import {
 	MetricsService,
 	FailureMetric,
 } from '@guardian/transcription-service-backend-common/src/metrics';
 import { SQSClient } from '@aws-sdk/client-sqs';
 import { setTimeout } from 'timers/promises';
-import { MAX_RECEIVE_COUNT } from '@guardian/transcription-service-common';
+// import { MAX_RECEIVE_COUNT } from '@guardian/transcription-service-common';
 import { checkSpotInterrupt } from './spot-termination';
 import { AutoScalingClient } from '@aws-sdk/client-auto-scaling';
 
@@ -145,7 +145,7 @@ const pollTranscriptionQueue = async (
 	instanceId: string,
 ) => {
 	const stage = config.app.stage;
-	const numberOfThreads = config.app.stage === 'PROD' ? 16 : 2;
+	// const numberOfThreads = config.app.stage === 'PROD' ? 16 : 2;
 	const isDev = config.app.stage === 'DEV';
 
 	logger.info(
@@ -239,223 +239,230 @@ const pollTranscriptionQueue = async (
 		return;
 	}
 
-	try {
-		// from this point all worker logs will have id & userEmail in their fields
-		logger.setCommonMetadata(job.id, job.userEmail);
-
-		const { outputBucketUrls, inputSignedUrl } = job;
-
-		logger.info(`Fetched transcription job with id ${job.id}`);
-
-		const destinationDirectory = isDev
-			? `${__dirname}/../../../worker-tmp-files`
-			: '/tmp';
-
-		const fileToTranscribe = await getObjectWithPresignedUrl(
-			inputSignedUrl,
-			job.id,
-			destinationDirectory,
-		);
-
-		const useContainer = job.engine !== 'whisperx';
-
-		const ffmpegDir = useContainer ? CONTAINER_FOLDER : destinationDirectory;
-
-		const fileName = path.basename(fileToTranscribe);
-		const filePath = `${ffmpegDir}/${fileName}`;
-		const wavPath = `${ffmpegDir}/${fileName}-converted.wav`;
-		logger.info(`Input file path: ${filePath}, Output file path: ${wavPath}`);
-
-		const ffmpegParams = getFfmpegParams(filePath, wavPath);
-
-		// docker container to run ffmpeg and whisper on file
-		const containerId = useContainer
-			? await getOrCreateContainer(path.parse(fileToTranscribe).dir)
-			: undefined;
-
-		const ffmpegResult = await runFfmpeg(ffmpegParams, containerId);
-
-		if (ffmpegResult === undefined) {
-			// when ffmpeg fails to transcribe, move message to the dead letter
-			// queue
-			if (!isDev && config.app.deadLetterQueueUrl) {
-				logger.error(
-					`'ffmpeg failed, moving message with message id ${taskMessage.MessageId} to dead letter queue`,
-				);
-				await moveMessageToDeadLetterQueue(
-					sqsClient,
-					taskQueueUrl,
-					config.app.deadLetterQueueUrl,
-					taskMessage.Body,
-					receiptHandle,
-					job.id,
-				);
-				logger.info(
-					`moved message with message id ${taskMessage.MessageId} to dead letter queue.`,
-				);
-			} else {
-				logger.info('skip moving message to dead letter queue in DEV');
-			}
-			await publishTranscriptionOutputFailure(
-				sqsClient,
-				config.app.destinationQueueUrls[job.transcriptDestinationService],
-				job,
-			);
-			return;
-		}
-
-		// Giant doesn't know the language of files uploaded to it, so for Giant files we first run language detection
-		// then based on the output, either run transcription or run transcription and translation, and return the output
-		// of both to the user. This is different from the transcription-service, where transcription and translation are
-		// two separate jobs
-		const combineTranscribeAndTranslate =
-			job.transcriptDestinationService === DestinationService.Giant &&
-			job.translate;
-		const extraTranslationTimMultiplier = combineTranscribeAndTranslate ? 2 : 1;
-
-		if (ffmpegResult.duration && ffmpegResult.duration !== 0) {
-			// Transcription time is usually slightly longer than file duration.
-			// Update visibility timeout to 2x the file duration plus 10 minutes for the model to load.
-			// This should avoid another worker picking up the task and to allow
-			// this worker to delete the message when it's finished.
-			await changeMessageVisibility(
-				sqsClient,
-				taskQueueUrl,
-				receiptHandle,
-				(ffmpegResult.duration * 2 + 600) * extraTranslationTimMultiplier,
-			);
-		}
-
-		const whisperBaseParams: WhisperBaseParams = {
-			containerId,
-			wavPath: wavPath,
-			file: fileToTranscribe,
-			numberOfThreads,
-			// whisperx always runs on powerful gpu instances so let's always use the medium model
-			model:
-				job.engine !== 'whisperx' && config.app.stage !== 'PROD'
-					? 'tiny'
-					: 'medium',
-			engine: job.engine,
-			diarize: job.diarize,
-			stage: config.app.stage,
-		};
-
-		const transcriptResult = await getTranscriptionText(
-			whisperBaseParams,
-			job.languageCode,
-			job.translate,
-			combineTranscribeAndTranslate,
-			job.engine === 'whisperx',
-		);
-
-		const languageCode: OutputLanguageCode =
-			job.languageCode === 'auto'
-				? transcriptResult.metadata.detectedLanguageCode
-				: job.languageCode;
-
-		// if we've received an interrupt signal we don't want to perform a half-finished transcript upload/publish as
-		// this may, for example, result in duplicate emails to the user. Here we assume that we can upload some text
-		// files to s3 and make a single request to SNS and SQS within 20 seconds
-		if (
-			INTERRUPTION_TIME &&
-			INTERRUPTION_TIME.getTime() - new Date().getTime() < 20 * 1000
-		) {
-			logger.warn('Spot termination happening soon, abandoning transcription');
-			// exit cleanly to prevent systemd restarting the process
-			process.exit(0);
-		}
-
-		await uploadAllTranscriptsToS3(
-			outputBucketUrls,
-			transcriptResult.transcripts,
-		);
-
-		if (
-			combineTranscribeAndTranslate &&
-			transcriptResult.transcriptTranslations &&
-			job.translationOutputBucketUrls
-		) {
-			await uploadAllTranscriptsToS3(
-				job.translationOutputBucketUrls,
-				transcriptResult.transcriptTranslations,
-			);
-		}
-
-		const outputBucketKeys: OutputBucketKeys = {
-			srt: outputBucketUrls.srt.key,
-			json: outputBucketUrls.json.key,
-			text: outputBucketUrls.text.key,
-		};
-
-		const transcriptionOutput: TranscriptionOutputSuccess = {
-			id: job.id,
-			status: 'SUCCESS',
-			languageCode,
-			userEmail: job.userEmail,
-			originalFilename: job.originalFilename,
-			outputBucketKeys,
-			translationOutputBucketKeys: job.translationOutputBucketUrls &&
-				transcriptResult.transcriptTranslations && {
-					srt: job.translationOutputBucketUrls.srt.key,
-					json: job.translationOutputBucketUrls.json.key,
-					text: job.translationOutputBucketUrls.text.key,
-				},
-			isTranslation: job.translate,
-			duration: ffmpegResult.duration,
-		};
-
-		await publishTranscriptionOutput(
-			sqsClient,
-			config.app.destinationQueueUrls[job.transcriptDestinationService],
-			transcriptionOutput,
-		);
-
-		logger.info(
-			`Worker successfully transcribed the file and sent notification to ${job.transcriptDestinationService} output queue`,
-			{
-				id: transcriptionOutput.id,
-				filename: transcriptionOutput.originalFilename,
-				userEmail: transcriptionOutput.userEmail,
-				mediaDurationSeconds: ffmpegResult.duration || 0,
-				specifiedLanguageCode: job.languageCode,
-				...transcriptResult.metadata,
-			},
-		);
-
-		logger.info(`Deleting message ${taskMessage.MessageId}`);
-		await deleteMessage(sqsClient, taskQueueUrl, receiptHandle);
-	} catch (error) {
-		const msg = 'Worker failed to complete';
-		logger.error(msg, error);
-		// Terminate the message visibility timeout
-		await changeMessageVisibility(sqsClient, taskQueueUrl, receiptHandle, 0);
-
-		// the type of ApproximateReceiveCount is string | undefined so need to
-		// handle the case where its missing. use default value
-		// MAX_RECEIVE_COUNT since its probably better to send too many failure
-		// messages than to not send any.
-		const defaultReceiveCount = MAX_RECEIVE_COUNT.toString();
-		const receiveCount = parseInt(
-			taskMessage.Attributes?.ApproximateReceiveCount || defaultReceiveCount,
-		);
-		if (receiveCount >= MAX_RECEIVE_COUNT) {
-			await publishTranscriptionOutputFailure(
-				sqsClient,
-				config.app.destinationQueueUrls[job.transcriptDestinationService],
-				job,
-			);
-		}
-	} finally {
-		logger.resetCommonMetadata();
-		await updateScaleInProtection(
-			autoScalingClient,
-			stage,
-			false,
-			instanceId,
-			asgName,
-		);
-	}
+	await publishTranscriptionOutputFailure(
+		sqsClient,
+		config.app.destinationQueueUrls[job.transcriptDestinationService],
+		job,
+	);
+	return;
+	//
+	// try {
+	// 	// from this point all worker logs will have id & userEmail in their fields
+	// 	logger.setCommonMetadata(job.id, job.userEmail);
+	//
+	// 	const { outputBucketUrls, inputSignedUrl } = job;
+	//
+	// 	logger.info(`Fetched transcription job with id ${job.id}`);
+	//
+	// 	const destinationDirectory = isDev
+	// 		? `${__dirname}/../../../worker-tmp-files`
+	// 		: '/tmp';
+	//
+	// 	const fileToTranscribe = await getObjectWithPresignedUrl(
+	// 		inputSignedUrl,
+	// 		job.id,
+	// 		destinationDirectory,
+	// 	);
+	//
+	// 	const useContainer = job.engine !== 'whisperx';
+	//
+	// 	const ffmpegDir = useContainer ? CONTAINER_FOLDER : destinationDirectory;
+	//
+	// 	const fileName = path.basename(fileToTranscribe);
+	// 	const filePath = `${ffmpegDir}/${fileName}`;
+	// 	const wavPath = `${ffmpegDir}/${fileName}-converted.wav`;
+	// 	logger.info(`Input file path: ${filePath}, Output file path: ${wavPath}`);
+	//
+	// 	const ffmpegParams = getFfmpegParams(filePath, wavPath);
+	//
+	// 	// docker container to run ffmpeg and whisper on file
+	// 	const containerId = useContainer
+	// 		? await getOrCreateContainer(path.parse(fileToTranscribe).dir)
+	// 		: undefined;
+	//
+	// 	const ffmpegResult = await runFfmpeg(ffmpegParams, containerId);
+	//
+	// 	if (ffmpegResult === undefined) {
+	// 		// when ffmpeg fails to transcribe, move message to the dead letter
+	// 		// queue
+	// 		if (!isDev && config.app.deadLetterQueueUrl) {
+	// 			logger.error(
+	// 				`'ffmpeg failed, moving message with message id ${taskMessage.MessageId} to dead letter queue`,
+	// 			);
+	// 			await moveMessageToDeadLetterQueue(
+	// 				sqsClient,
+	// 				taskQueueUrl,
+	// 				config.app.deadLetterQueueUrl,
+	// 				taskMessage.Body,
+	// 				receiptHandle,
+	// 				job.id,
+	// 			);
+	// 			logger.info(
+	// 				`moved message with message id ${taskMessage.MessageId} to dead letter queue.`,
+	// 			);
+	// 		} else {
+	// 			logger.info('skip moving message to dead letter queue in DEV');
+	// 		}
+	// 		await publishTranscriptionOutputFailure(
+	// 			sqsClient,
+	// 			config.app.destinationQueueUrls[job.transcriptDestinationService],
+	// 			job,
+	// 		);
+	// 		return;
+	// 	}
+	//
+	// 	// Giant doesn't know the language of files uploaded to it, so for Giant files we first run language detection
+	// 	// then based on the output, either run transcription or run transcription and translation, and return the output
+	// 	// of both to the user. This is different from the transcription-service, where transcription and translation are
+	// 	// two separate jobs
+	// 	const combineTranscribeAndTranslate =
+	// 		job.transcriptDestinationService === DestinationService.Giant &&
+	// 		job.translate;
+	// 	const extraTranslationTimMultiplier = combineTranscribeAndTranslate ? 2 : 1;
+	//
+	// 	if (ffmpegResult.duration && ffmpegResult.duration !== 0) {
+	// 		// Transcription time is usually slightly longer than file duration.
+	// 		// Update visibility timeout to 2x the file duration plus 10 minutes for the model to load.
+	// 		// This should avoid another worker picking up the task and to allow
+	// 		// this worker to delete the message when it's finished.
+	// 		await changeMessageVisibility(
+	// 			sqsClient,
+	// 			taskQueueUrl,
+	// 			receiptHandle,
+	// 			(ffmpegResult.duration * 2 + 600) * extraTranslationTimMultiplier,
+	// 		);
+	// 	}
+	//
+	// 	const whisperBaseParams: WhisperBaseParams = {
+	// 		containerId,
+	// 		wavPath: wavPath,
+	// 		file: fileToTranscribe,
+	// 		numberOfThreads,
+	// 		// whisperx always runs on powerful gpu instances so let's always use the medium model
+	// 		model:
+	// 			job.engine !== 'whisperx' && config.app.stage !== 'PROD'
+	// 				? 'tiny'
+	// 				: 'medium',
+	// 		engine: job.engine,
+	// 		diarize: job.diarize,
+	// 		stage: config.app.stage,
+	// 	};
+	//
+	// 	const transcriptResult = await getTranscriptionText(
+	// 		whisperBaseParams,
+	// 		job.languageCode,
+	// 		job.translate,
+	// 		combineTranscribeAndTranslate,
+	// 		job.engine === 'whisperx',
+	// 	);
+	//
+	// 	const languageCode: OutputLanguageCode =
+	// 		job.languageCode === 'auto'
+	// 			? transcriptResult.metadata.detectedLanguageCode
+	// 			: job.languageCode;
+	//
+	// 	// if we've received an interrupt signal we don't want to perform a half-finished transcript upload/publish as
+	// 	// this may, for example, result in duplicate emails to the user. Here we assume that we can upload some text
+	// 	// files to s3 and make a single request to SNS and SQS within 20 seconds
+	// 	if (
+	// 		INTERRUPTION_TIME &&
+	// 		INTERRUPTION_TIME.getTime() - new Date().getTime() < 20 * 1000
+	// 	) {
+	// 		logger.warn('Spot termination happening soon, abandoning transcription');
+	// 		// exit cleanly to prevent systemd restarting the process
+	// 		process.exit(0);
+	// 	}
+	//
+	// 	await uploadAllTranscriptsToS3(
+	// 		outputBucketUrls,
+	// 		transcriptResult.transcripts,
+	// 	);
+	//
+	// 	if (
+	// 		combineTranscribeAndTranslate &&
+	// 		transcriptResult.transcriptTranslations &&
+	// 		job.translationOutputBucketUrls
+	// 	) {
+	// 		await uploadAllTranscriptsToS3(
+	// 			job.translationOutputBucketUrls,
+	// 			transcriptResult.transcriptTranslations,
+	// 		);
+	// 	}
+	//
+	// 	const outputBucketKeys: OutputBucketKeys = {
+	// 		srt: outputBucketUrls.srt.key,
+	// 		json: outputBucketUrls.json.key,
+	// 		text: outputBucketUrls.text.key,
+	// 	};
+	//
+	// 	const transcriptionOutput: TranscriptionOutputSuccess = {
+	// 		id: job.id,
+	// 		status: 'SUCCESS',
+	// 		languageCode,
+	// 		userEmail: job.userEmail,
+	// 		originalFilename: job.originalFilename,
+	// 		outputBucketKeys,
+	// 		translationOutputBucketKeys: job.translationOutputBucketUrls &&
+	// 			transcriptResult.transcriptTranslations && {
+	// 				srt: job.translationOutputBucketUrls.srt.key,
+	// 				json: job.translationOutputBucketUrls.json.key,
+	// 				text: job.translationOutputBucketUrls.text.key,
+	// 			},
+	// 		isTranslation: job.translate,
+	// 		duration: ffmpegResult.duration,
+	// 	};
+	//
+	// 	await publishTranscriptionOutput(
+	// 		sqsClient,
+	// 		config.app.destinationQueueUrls[job.transcriptDestinationService],
+	// 		transcriptionOutput,
+	// 	);
+	//
+	// 	logger.info(
+	// 		`Worker successfully transcribed the file and sent notification to ${job.transcriptDestinationService} output queue`,
+	// 		{
+	// 			id: transcriptionOutput.id,
+	// 			filename: transcriptionOutput.originalFilename,
+	// 			userEmail: transcriptionOutput.userEmail,
+	// 			mediaDurationSeconds: ffmpegResult.duration || 0,
+	// 			specifiedLanguageCode: job.languageCode,
+	// 			...transcriptResult.metadata,
+	// 		},
+	// 	);
+	//
+	// 	logger.info(`Deleting message ${taskMessage.MessageId}`);
+	// 	await deleteMessage(sqsClient, taskQueueUrl, receiptHandle);
+	// } catch (error) {
+	// 	const msg = 'Worker failed to complete';
+	// 	logger.error(msg, error);
+	// 	// Terminate the message visibility timeout
+	// 	await changeMessageVisibility(sqsClient, taskQueueUrl, receiptHandle, 0);
+	//
+	// 	// the type of ApproximateReceiveCount is string | undefined so need to
+	// 	// handle the case where its missing. use default value
+	// 	// MAX_RECEIVE_COUNT since its probably better to send too many failure
+	// 	// messages than to not send any.
+	// 	const defaultReceiveCount = MAX_RECEIVE_COUNT.toString();
+	// 	const receiveCount = parseInt(
+	// 		taskMessage.Attributes?.ApproximateReceiveCount || defaultReceiveCount,
+	// 	);
+	// 	if (receiveCount >= MAX_RECEIVE_COUNT) {
+	// 		await publishTranscriptionOutputFailure(
+	// 			sqsClient,
+	// 			config.app.destinationQueueUrls[job.transcriptDestinationService],
+	// 			job,
+	// 		);
+	// 	}
+	// } finally {
+	// 	logger.resetCommonMetadata();
+	// 	await updateScaleInProtection(
+	// 		autoScalingClient,
+	// 		stage,
+	// 		false,
+	// 		instanceId,
+	// 		asgName,
+	// 	);
+	// }
 };
 
 main();
