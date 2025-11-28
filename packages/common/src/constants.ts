5410dd93914c453c859319a4cff9f0aaa28a8677
philmcmahon <philip.mcmahon@theguardian.com>
2025-11-28 16:54:48 +0000

    Add global ABOUT_THIS_TOOL_YOUTUBE constant linking directly to the youtube bit of the google doc

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -2,3 +2,6 @@ export const MAX_RECEIVE_COUNT = 3;
 
 export const ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60;
 export const TWELVE_HOURS_IN_SECONDS = 12 * 60 * 60;
+
+export const ABOUT_THIS_TOOL_YOUTUBE =
+	'https://docs.google.com/document/d/1e224Fe5tJJNeLBNvYLVJ4FWd_O-1nwrJcqKyEXI03lA/edit?tab=t.0#heading=h.9ldpalu9ztlj';
