b12274ed85a5283c5d3c5dd3c1d2f4c662a3dbc8
philmcmahon <philip.mcmahon@theguardian.com>
2025-12-05 12:44:06 +0000

    Improve testMessage

--- packages/webpage-snapshot/test/testMessage.ts
+++ packages/webpage-snapshot/test/testMessage.ts
@@ -16,7 +16,7 @@ export const getTestMessage = async () => {
 				messageId: 'id123',
 				ReceiptHandle: 'abc123',
 				MD5OfBody: 'md5md5',
-				body: `{"id":"483be6b2-f0f8-4ba2-8416-35e0c0a0f4a3","url":"https://www.theguardian.com/","client":"EXTERNAL","outputQueueUrl":"","mediaDownloadId":"media-download-id-123","webpageSnapshotId":"webpage-snapshot-id-456","mediaDownloadOutputSignedUrl":"","webpageSnapshotOutputSignedUrl":"${signedUrl}"}`,
+				body: `{"id":"483be6b2-f0f8-4ba2-8416-35e0c0a0f4a3","mediaDownloadId":"mediaid","webpageSnapshotId":"webid","url":"https://www.theguardian.com","client":"EXTERNAL","outputQueueUrl":"","webpageSnapshotOutputSignedUrl":"${signedUrl}","mediaDownloadOutputSignedUrl":"${signedUrl}"}`,
 			},
 		],
 	};
