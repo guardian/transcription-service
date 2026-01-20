f83a57700277beafa833327512b9159e619769a3
philmcmahon <philip.mcmahon@theguardian.com>
2026-01-20 11:39:27 +0000

    Drop reservation time by an hour now that workers don't need to warm up

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -363,7 +363,7 @@ const pollTranscriptionQueue = async (
 
 		if (ffmpegResult.duration && ffmpegResult.duration !== 0) {
 			// Transcription time is usually slightly longer than file duration.
-			// Update visibility timeout to 2x the file duration plus 1 hour for the model to load.
+			// Update visibility timeout to 2x the file duration
 			// (TODO: investigate whisperx model load time/transcription performance further - it seems to vary)
 			// This should avoid another worker picking up the task and to allow
 			// this worker to delete the message when it's finished.
@@ -371,7 +371,7 @@ const pollTranscriptionQueue = async (
 				sqsClient,
 				taskQueueUrl,
 				receiptHandle,
-				(ffmpegResult.duration * 2 + 3_600) * extraTranslationTimeMultiplier,
+				ffmpegResult.duration * 2 * extraTranslationTimeMultiplier,
 			);
 		}
 
