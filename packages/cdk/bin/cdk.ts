f984f3604ef16e071516fbd12f98d57344a27186
Tom Richards <tom.richards@guardian.co.uk>
2026-08-20 09:19:04 +0100

    change cdk snapshotting to be the actual infra to be created, rather than `TEST` stage (in doing so capture all of it not just `TranscriptionService`)

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -4,33 +4,39 @@ import { TranscriptionServiceRepository } from '../lib/repository';
 import { TranscriptionService } from '../lib/transcription-service';
 import { TranscriptionServiceUniversalInfra } from '../lib/universal-infra';
 
+const stack = 'investigations';
+const env = { region: 'eu-west-1' };
+
 const app = new GuRoot();
-new TranscriptionService(app, 'TranscriptionService-CODE', {
-	stack: 'investigations',
-	stage: 'CODE',
-	env: { region: 'eu-west-1' },
-});
-new TranscriptionService(app, 'TranscriptionService-PROD', {
-	stack: 'investigations',
-	stage: 'PROD',
-	env: { region: 'eu-west-1' },
-});
 
-// repository will be shared between CODE and PROD so needs to be a separate stack
-new TranscriptionServiceRepository(app, 'TranscriptionServiceRepository', {
-	stack: 'investigations',
-	stage: 'PROD',
-	env: { region: 'eu-west-1' },
-});
-
-// This is another stack which is used for both code/prod - but as repository already existed I made a new stack to avoid
-// having to delete the whole repository stack (including all containers) in order to give it a less specific name
-new TranscriptionServiceUniversalInfra(
-	app,
-	'TranscriptionServiceUniversalInfra',
-	{
-		stack: 'investigations',
+export const guStacks = [
+	new TranscriptionService(app, 'TranscriptionService-CODE', {
+		stack,
+		stage: 'CODE',
+		env,
+	}),
+	new TranscriptionService(app, 'TranscriptionService-PROD', {
+		stack,
 		stage: 'PROD',
-		env: { region: 'eu-west-1' },
-	},
-);
+		env,
+	}),
+
+	// repository will be shared between CODE and PROD so needs to be a separate stack
+	new TranscriptionServiceRepository(app, 'TranscriptionServiceRepository', {
+		stack,
+		stage: 'PROD', // TODO probably ought to be INFRA?
+		env,
+	}),
+
+	// This is another stack which is used for both code/prod - but as repository already existed I made a new stack to avoid
+	// having to delete the whole repository stack (including all containers) in order to give it a less specific name
+	new TranscriptionServiceUniversalInfra(
+		app,
+		'TranscriptionServiceUniversalInfra',
+		{
+			stack,
+			stage: 'PROD', // TODO probably ought to be INFRA?
+			env,
+		},
+	),
+];
